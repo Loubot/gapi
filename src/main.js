111ba57eed8f89b822cf98df47916443410cea9b
Angelini <Lllouis@yahoo.com>
2018-05-12 14:23:56 +0100

    receiving data

--- src/main.js
+++ src/main.js
@@ -6,117 +6,13 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-const CLIENT_ID = '1024622221266-rjem367cr9i2gsmgp0968dunm9vvkgrs.apps.googleusercontent.com';
-// Array of API discovery doc URLs for APIs used by the quickstart
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
 
 
 new Vue({
   el: '#app',
-
-  data() {
-    return {
-      items: undefined,
-      api: undefined,
-      authorized: false
-    }
-  },
-
-  created() {
-    this.api = gapi;
-    this.handleClientLoad();
-  },
-
-  methods: {
-    /**
-     *  On load, called to load the auth2 library and API client library.
-     */
-    handleClientLoad() {
-      this.api.load('client:auth2', this.initClient);
-    },
-
-    /**
-     *  Initializes the API client library and sets up sign-in state
-     *  listeners.
-     */
-    initClient() {
-      let vm = this;
-
-      vm.api.client.init({
-        // apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-      }).then(_ => {
-        // Listen for sign-in state changes.
-        vm.api.auth2.getAuthInstance().isSignedIn.listen(vm.authorized);
-      });
-    },
-
-    /**
-     *  Sign in the user upon button click.
-     */
-    handleAuthClick(event) {
-      Promise.resolve(this.api.auth2.getAuthInstance().signIn())
-        .then(_ => {
-          this.authorized = true;
-        });
-    },
-
-    /**
-     *  Sign out the user upon button click.
-     */
-    handleSignoutClick(event) {
-      Promise.resolve(this.api.auth2.getAuthInstance().signOut())
-        .then(_ => {
-          this.authorized = false;
-        });
-    },
-
-    /**
-     * Print the summary and start datetime/date of the next ten events in
-     * the authorized user's calendar. If no events are found an
-     * appropriate message is printed.
-     */
-    getData() {
-      let vm = this;
-
-      vm.api.client.calendar.events.list({
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 10,
-        'orderBy': 'startTime'
-      }).then(response => {
-        vm.items = this.syntaxHighlight(response.result.items);
-        console.log(vm.items);
-      });
-    },
-
-    syntaxHighlight(json) {
-      if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
-      }
-      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match => {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-          if (/:$/.test(match)) {
-            cls = 'key';
-          } else {
-            cls = 'string';
-          }
-        } else if (/true|false/.test(match)) {
-          cls = 'boolean';
-        } else if (/null/.test(match)) {
-          cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-      });
-    }
-  }
+  router,
+  components: { App },
+  template: '<App/>',
+  
 });
